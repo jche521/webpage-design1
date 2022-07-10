19c0d658a3dedabba73cbbeb8f91aae045e99ee2
Jolin <jche521@aucklanduni.ac.nz>
2022-07-11 11:29:21 +1200

    more styling on nav bar, add menu icon, prep to work on js for navbar toggling

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbarButton = document.querySelector("icon")
+const navbar = document.querySelector("navbar")
+
+navbarButton.addEventListener("click", toggleNavBar)
+
+function toggleNavBar(){
+    if()
+}
