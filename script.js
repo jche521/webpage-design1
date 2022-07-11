4bd4d84b6e4f7c82849c230ad4ad5bf97a36d1f1
Jolin <jche521@aucklanduni.ac.nz>
2022-07-11 14:04:25 +1200

    navbar toggle complete

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const navbarButton = document.querySelector("icon")
-const navbar = document.querySelector("navbar")
+const navbarButton = document.querySelector("#icon");
+const navbar = document.querySelector(".navbar");
 
-navbarButton.addEventListener("click", toggleNavBar)
+navbarButton.addEventListener("click", toggleNavBar);
 
-function toggleNavBar(){
-    if()
+function toggleNavBar() {
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+    navbarButton.classList.replace("fa-xmark", "fa-bars");
+  } else {
+    navbar.classList.add("active");
+    navbarButton.classList.replace("fa-bars", "fa-xmark");
+  }
 }
